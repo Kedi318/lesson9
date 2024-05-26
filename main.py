c8f66c0893dbfda6978434615c2137a2ebd85173
Kedi318 <136179049+Kedi318@users.noreply.github.com>
2024-05-26 10:24:48 +0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,19 @@
+from flask import Flask
+import random
+facts_list = ["Teknolojik bağımlılıktan mustarip olan çoğu kişi, kendilerini şebeke kapsama alanı dışında bulduklarında veya cihazlarını kullanamadıkları zaman yoğun stres yaşarlar.", "2018 yılında yapılan bir araştırmaya göre 18-34 yaş arası kişilerin %50'den fazlası kendilerini akıllı telefonlarına bağımlı olarak görüyor.", "Teknolojik bağımlılık çalışması, modern bilimsel araştırmanın en ilgili alanlarından biridir.", "2019'da yapılan bir araştırmaya göre, insanların %60'ından fazlası akıllı telefonlarındaki iş mesajlarına işten ayrıldıktan sonraki 15 dakika içinde yanıt veriyor.", "Teknolojik bağımlılıkla mücadele etmenin bir yolu, zevk veren ve ruh halini iyileştiren faaliyetler aramaktır.", "Elon Musk, sosyal ağların içeriği görüntülemek için mümkün olduğunca fazla zaman harcamamız için bizi platformun içinde tutmak üzere tasarlandığını iddia ediyor.", "Elon Musk ayrıca sosyal ağların düzenlenmesini ve kullanıcıların kişisel verilerinin korunmasını savunmaktadır. Sosyal ağların hakkımızda büyük miktarda bilgi topladığını ve bu bilgilerin daha sonra düşüncelerimizi ve davranışlarımızı manipüle etmek için kullanılabileceğini iddia ediyor.", "Sosyal ağların olumlu ve olumsuz yanları vardır ve bu platformları kullanırken her ikisinin de farkında olmalıyız."]
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return f'<h1>Merhaba! Bu sayfada teknoloji bağımlılığı hakkında rastgele bir bilgi öğrenebilirsin!</h1> <a href="/rastgele_gercek">Rastgele bir bilgi öğren</a> <a href="/yazitura">________</a>'
+
+@app.route("/rastgele_gercek")
+def facts():
+    return f'<p>{random.choice(facts_list)}</p>'
+
+@app.route("/yazitura")
+def yazitura():
+    return f'<p>{random.choice(["Yazı", "Tura"])}</p>'
+
+app.run(debug=True)
